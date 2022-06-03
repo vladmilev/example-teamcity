cae3c0fc822150b2e46b92e11e4addf9fbee46cd
vladmilev <vlad_milev@mail.ru>
2022-06-03 18:36:50 +0700

    Update HelloPlayer.java

--- src/main/java/plaindoll/HelloPlayer.java
+++ src/main/java/plaindoll/HelloPlayer.java
@@ -4,6 +4,7 @@ public class HelloPlayer{
 	public static void main(String[] args) {
 		Welcomer welcomer = new Welcomer();
 		System.out.println(welcomer.sayWelcome());
+		System.out.println(welcomer.sayWelcome());
 		System.out.println(welcomer.sayFarewell());
 	}
 }
